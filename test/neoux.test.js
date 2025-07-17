52a857cc5db90ee4898a3f319f2f081a955c73db
Toshihiko Arai <araemonz@gmail.com>
2025-07-17 20:40:16 +0900

    IDEでテストが実行できるように修正

--- test/neoux.test.js
+++ test/neoux.test.js
@@ -1,3 +1,19 @@
+const { JSDOM } = require("jsdom");
+
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`, {
+    url: "http://localhost",
+    pretendToBeVisual: true, // requestAnimationFrame の有効化
+});
+
+global.window = dom.window;
+global.document = dom.window.document;
+global.HTMLElement = dom.window.HTMLElement;
+global.requestAnimationFrame = dom.window.requestAnimationFrame;
+global.cancelAnimationFrame = dom.window.cancelAnimationFrame;
+
+// // neouxのグローバル初期化
+// window.neoux = {};
+
 const {expect} = require("chai");
 const neoux = require("../js/neoux.js")
 
