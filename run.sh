6e320e1036ac86a58fa0b20029dc9c1b67bb392f
Toshihiko Arai <araemonz@gmail.com>
2025-07-17 20:01:31 +0900

    開発サーバー起動スクリプトの調整

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-live-server . --port=5200 --host=localhost --ignore=".git,.idea,run.sh"
+echo "Access for LAN at http://macbook.local:5200"
+
+live-server . --port=5200 --host=0.0.0.0 --no-browser --ignore=".git,.idea,run.sh"
+
+
