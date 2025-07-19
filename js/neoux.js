9bef20d37a23297cf52334daabe23cd06aa211ed
Toshihiko Arai <araemonz@gmail.com>
2025-07-19 16:08:38 +0900

    nu.segmentのリファクタリング

--- js/neoux.js
+++ js/neoux.js
@@ -392,27 +392,25 @@
          * segment要素を生成してDOMに追加
          * @param {string} mountId - 追加先の親要素
          * @param {Object} config - セグメント設定オブジェクト
-         * @param {string} config.id - div要素のid
          * @param {string} config.name - ラジオグループ名
          * @param {Array.<{label: string, value: string}>} config.options - 選択肢の配列
          * @param {string} config.value - 初期選択値
          * @param {Function} callback - 選択時のコールバック（引数: value）
          */
         window.nu.segment = function (mountId, config, callback) {
-            const { id, name, options, value: initialValue } = config;
             const container = document.createElement("div");
             container.className = "nuSegment";
-            container.id = id;
+            container.id = mountId + "_segment";
 
-            options.forEach(opt => {
-                const inputId = `${id}_${opt.value}`;
+            config.options.forEach(opt => {
+                const inputId = `${container.id}_${opt.value}`;
 
                 const input = document.createElement("input");
                 input.type = "radio";
-                input.name = name;
+                input.name = config.name;
                 input.id = inputId;
                 input.value = opt.value;
-                if (opt.value === initialValue) input.checked = true;
+                if (opt.value === config.value) input.checked = true;
 
                 const label = document.createElement("label");
                 label.setAttribute("for", inputId);
