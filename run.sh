e731a37337fa27cb9cb05b8689d48970e50205b8
Toshihiko Arai <araemonz@gmail.com>
2025-08-10 13:28:01 +0900

    runスクリプトでmodules配下のjs変更を除外

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 echo "Access for LAN at http://macbook.local:5200"
 
-live-server . --port=5200 --host=0.0.0.0 --no-browser --ignore=".git,.idea,run.sh"
+live-server . --port=5200 --host=0.0.0.0 --no-browser --ignore=".git,.idea,run.sh,src/nu/js/modules/**"
+
 
 
